3fba0f74bdc6d07286aa43f5b4df9986f3b444d8
dvahana2424-web <dvahana2424@gmail.com>
2026-04-14 02:14:59 +0800

    Update otherfunction.ps1

--- otherfunction.ps1
+++ otherfunction.ps1
@@ -1,3 +1,9 @@
+# Load the Windows Forms assembly to enable message boxes
+Add-Type -AssemblyName System.Windows.Forms
+
+# Show the notification pop-up
+[System.Windows.Forms.MessageBox]::Show("I will send you to YouTube.", "Notification", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+
 # Define the YouTube URL
 $youtubeLink = "https://youtu.be/CoKIphT7xrI?si=DY-IiMF0fc0u-bMH"
 
